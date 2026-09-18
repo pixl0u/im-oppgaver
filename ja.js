c3c5f2fe9dbff03f05ea1b745bddd40963c81c75
pixl0u <langhofflouis@gmail.com>
2026-09-18 08:57:22 +0200

    commit

---
+++ ja.js
@@ -0,0 +1,42 @@
+
+// let frukt = ["eple", "banan", "kiwi"];
+// console.log(frukt);
+// console.log(frukt[0]);
+// console.log(frukt[1]);
+// console.log(frukt[2]);
+// frukt.push("pære");
+// console.log(frukt[3]);
+// console.log(frukt.length);
+// frukt[2]='appelsin'
+// console.log(frukt);
+// sum=0
+// let handleliste = ["-melk", "-brød", "-ost", "-egg"];
+// handleliste.push('-hele butikken')
+// handleliste.push('-nesten hele butikken')
+// console.log('handleliste:');
+// for (let vare of handleliste) {
+//     sum += 1
+// console.log(sum,vare);
+// }
+// let temperaturer = [12, 14, 11, 9, 15, 17, 13];
+// let sum = 0;
+// dum = 0
+// boon = Math.max(temperaturer)
+// doon = Math.min(temperaturer)
+// for (let temperatur of temperaturer) {
+
+// sum = sum + temperatur;
+// if (temperatur >= 13){
+//     dum +=1
+// }
+
+// }
+// goon =sum / temperaturer.length
+
+// console.log(goon);
+// console.log(sum);
+// console.log(dum);
+// console.log(boon);
+// console.log(doon);
+
+let poeng = [72, 45, 91, 63, 38, 84, 56];
